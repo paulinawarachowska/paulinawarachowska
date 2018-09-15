683e098feedbac7de364bf6b64049ae982583639
Karol Sawicki <karol.sawicki@gazeta.pl>
2018-09-15 05:12:29 +0200

    thirt commit

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,10 @@
 // Animate Smooth Scroll
 $('#view-work').on('click', function() {
-  const images = $('#Najlepszy-Fryzjer-W-Pruszkowie').position().top;
+  const NajlepszyFryzjerWPruszkowie = $('#NajlepszyFryzjerWPruszkowie').position().top;
 
   $('html, body').animate(
     {
-      scrollTop: images;
+      scrollTop: NajlepszyFryzjerWPruszkowie
     },
     900
   );
